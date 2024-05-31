976ed569b5b98dda7f4821a530d30a247e406610
Philani Antony Dlamini <philaniantonydlamini@gmail.com>
2024-05-31 22:33:38 +0200

    Create pages and routes

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <h1 className='text-red-500'>Home Page</h1>
+      <h1 className='text-red-500'>Posts Page</h1>
     </div>
   )
 }
