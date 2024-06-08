5ab0208bc61a69ff52d4c99c81867ab5e905ee74
Philani Antony Dlamini <philaniantonydlamini@gmail.com>
2024-06-08 23:14:43 +0200

    Update layout and News component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,13 +7,13 @@ export default function Sidebar() {
   return (
     <div className='flex flex-col gap-4 p-3'>
       <Link href='/'>
-        <FaXTwitter className='w-16 h-16 cursor-pointer  p-3 hover:bg-gray-100 rounded-full transition-all duration-200' />
+        <FaXTwitter className="w-16 h-16 cursor-pointer  p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
       </Link>
-      <Link href='/' className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'>
+      <Link href='/' className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit">
         <HiHome className='w-7 h-7' />
         <span className='font-bold hidden xl:inline'>Home</span>
       </Link>
-      <button className='bg-blue-400 text-white rounded-full hover:bg-brightness-95 transition-all duration-200 w-48 h-8 hidden xl:inline'>
+      <button className="bg-blue-400 text-white rounded-full hover:bg-brightness-95 transition-all duration-200 w-48 h-8 hidden xl:inline font-semibold">
         Sign In
       </button>
     </div>
